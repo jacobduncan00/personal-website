5bdcf7dfb3602898a77ec25342c99de7e2be90db
jacobduncan00 <jacobduncan00@yahoo.com>
2023-09-06 19:23:25 -0500

    refactor

--- components/PictureGallery.tsx
+++ components/PictureGallery.tsx
@@ -7,7 +7,7 @@ const GalleryCard = ({ section }: { section: string }) => {
       className="col-span-3 md:col-span-2 bg-white dark:bg-gray-900
 			 rounded-3xl text-gray-600 dark:text-gray-300 aspect-2/1 pb-4 md:pb-0 flex relative overflow-hidden"
       whileHover="groupHover"
-      animate={{ opacity: ["all"].includes(section) ? 1 : 1 }}
+      animate={{ opacity: ["all", "about"].includes(section) ? 1 : 0.3 }}
       variants={{
         groupHover: {
           scale: 1.01,
